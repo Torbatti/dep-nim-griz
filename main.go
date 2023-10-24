ea71cb91b08d5fdc06963ac81dc54dcf4603a38e
torbatti <torbattii@gmail.com>
2023-10-25 00:27:40 +0330

    update: added makeApp function

--- main.go
+++ main.go
@@ -5,36 +5,67 @@ import (
 	"net/http"
 	"os"
 
+	// Insiders
+	"github.com/torbatti/nim-griz/middlewares"
+	"github.com/torbatti/nim-griz/routes"
+
 	// chi related
 	"github.com/go-chi/chi"
-	"github.com/torbatti/nim-griz/middlewares"
 
 	// .env
 	"github.com/joho/godotenv"
+
+	// Gorm
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type App struct {
+	Router *chi.Mux
+	Db     *gorm.DB
+}
+
+func makeApp() *App {
+	app := &App{}
+
+	// Database: Opening
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database .\n ", err.Error())
+		os.Exit(2)
+	} // db.Logger = logger.Default.LogMode(logger.Info)
+
+	// Database: Migrations
+	db.AutoMigrate()
+
+	// Connecting
+	app.Router = chi.NewRouter()
+	app.Db = db
+
+	return app
+}
+
 func main() {
+	app := makeApp()
+
 	// Setting Up .env
 	godotenv.Load(".env") //os.Setenv(port, "8000")
 	portString := os.Getenv("PORT")
 
-	// Setting Chi Router
-	router := chi.NewRouter()
+	// MiddleWares // NOTE: all middlewares must be defined before routes on a mux
+	middlewares.Base(app.Router)
 
-	// MiddleWares
-	// NOTE: all middlewares must be defined before routes on a mux
-	middlewares.Base(router)
+	// Root
+	root := chi.NewRouter()
+	app.Router.Mount("/", root)
+	root.Get("/hi", routes.HelloWorld)
 
-	// V1 Router
-	v1Router := chi.NewRouter()
-	router.Mount("/v1", v1Router)
+	api := chi.NewRouter() // NOTE: Use mux Then Mount
+	root.Mount("/api", api)
 
-	// Setting Up app server
-	app := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
-	}
 	// Static Files : Fonts/JsLibs/Imgs/etc.
+
 	// View Routes
 	// http.Handle("/foo", routes.Base(http.ResponseWriter, *http.Request))
 	// BackEnd Routes
@@ -42,8 +73,14 @@ func main() {
 	// Hx Routes
 	// Utils
 
+	// Initial server
+	server := &http.Server{
+		Handler: app.Router,
+		Addr:    ":" + portString,
+	}
+
 	// Listen And Serve
-	err := app.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
