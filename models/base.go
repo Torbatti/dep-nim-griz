b3abdeb3e40027f84f355b728809128b2c4799d7
torbatti <torbattii@gmail.com>
2023-10-26 16:11:01 +0330

    initial base models

---
+++ models/base.go
@@ -0,0 +1,33 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string `gorm:"primaryKey"`
+	CreatedAt time.Time
+	Name      string `json:"User"`
+	Lists     []List
+}
+
+type Game struct {
+	ID            string `gorm:"primaryKey"`
+	CreatedAt     time.Time
+	Name          string `json:"Game"`
+	Year          uint   `json:"Year"`
+	Publisher     string `json:"Publisher"`
+	Developer     string `json:"Dev"`
+	Platform      string `json:"Platform"`
+	GameLink      string `json:"GameLink"`
+	DevLink       string `json:"DevLink"`
+	PublisherLink string `json:"PublisherLink"`
+	PlatformLink  string `json:"PlatformLink"`
+}
+
+type List struct {
+	ID        string `gorm:"primaryKey"`
+	CreatedAt time.Time
+	Name      string `json:"List"`
+	Games     []Game
+}
+
+// SAMPLE  string `json:""`
