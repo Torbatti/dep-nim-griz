5ef841fe133c5007662cf46c5328ab212d595255
torbatti <torbattii@gmail.com>
2023-10-29 20:56:23 +0330

    update: added many2many association

--- models/base.go
+++ models/base.go
@@ -3,31 +3,32 @@ package models
 import "time"
 
 type User struct {
-	ID        string `json:"id" gorm:"primaryKey"`
+	ID        uint `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time
 	Name      string `json:"User"`
-	Lists     []List
+	Lists     []List `gorm:"foreignKey:UserRefer"`
 }
 
 type Game struct {
-	ID            string `json:"id" gorm:"primaryKey"`
+	ID            uint `json:"Id" gorm:"primaryKey"`
 	CreatedAt     time.Time
-	Name          string `json:"Game"`
-	Year          uint   `json:"Year"`
-	Publisher     string `json:"Publisher"`
-	Developer     string `json:"Dev"`
-	Platform      string `json:"Platform"`
-	GameLink      string `json:"GameLink"`
-	DevLink       string `json:"DevLink"`
-	PublisherLink string `json:"PublisherLink"`
-	PlatformLink  string `json:"PlatformLink"`
+	Name          string  `json:"Game"`
+	Year          uint    `json:"Year"`
+	Publisher     string  `json:"Publisher"`
+	Developer     string  `json:"Dev"`
+	Platform      string  `json:"Platform"`
+	GameLink      string  `json:"GameLink"`
+	DevLink       string  `json:"DevLink"`
+	PublisherLink string  `json:"PublisherLink"`
+	PlatformLink  string  `json:"PlatformLink"`
+	Lists         []*List `gorm:"many2many:game_lists;"`
 }
 
 type List struct {
-	ID        string `json:"id" gorm:"primaryKey"`
+	ID        uint `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time
 	Name      string `json:"List"`
-	Games     []Game
+	Public    bool   `json:"Public"`
+	UserRefer uint
+	Games     []*Game `gorm:"many2many:game_lists;"`
 }
-
-// SAMPLE  string `json:""`
